ad712ed071e00cb6d062fb4cb917c182bd99b841
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-02-02 20:28:26 -0500

    Task 3 - Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -5,10 +5,8 @@ if (purchaseAmount > 100) {
     let discount = 0.10 ;                       // Discount percentage / 10% 
     let finalAmount = purchaseAmount - (purchaseAmount * discount) ;
     
-    console.log(`Your purchase qualifies for discount! your new total after discount is: $${finalAmount}`) ;
-    } else {
-    console.log(`Your total without any discount is: $${finalAmount}`) ;    
-    } ;                                         // If-else condition statement for discount eligibility
+    console.log(`Your purchase qualifies for discount! your new total after discount is: $${finalAmount}`) ; 
+    } ;                                         // If condition statement for discount eligibility
                                                 // Oputput: Your purchase qualifies for discount! your new total after discount is: $180
 
 
@@ -20,4 +18,13 @@ for (let i = 0; i < sales.length; i++) {
     totalSales += sales[i];   
 }                                               // For loop calculation for total sales
 
-console.log(`Total Sales: $${totalSales}`) ;       // Output: Total Sales: $1343
+console.log(`Total Sales: $${totalSales}`) ;    // Output: Total Sales: $1343
+
+
+// Task 3 - Inventory Depletion
+
+let stock = 10                                  // Creation of stock account starting at 10
+while (stock >= 0) {
+    console.log (`Inventory stock is: ${stock}`) ; stock--      // While stock is greater than zero, decrease it until it reaches zero
+}                                               // Output: Inventory stock is: 10, Inventory stock is: 9, ...
+
